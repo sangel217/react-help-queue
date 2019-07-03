bc609bf248ec825508581b410b36050d6858916d
Student <example@example.com>
2019-07-03 11:37:00 -0700

    add routing and 404 error page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
+import { Switch, Route } from 'react-router-dom';
+import NewTicketForm from './NewTicketForm';
+import Error404 from './Error404';
 
 function App(){
   return (
     <div>
       <Header/>
-      <TicketList/>
-      
+      <Switch>
+        <Route exact path='/' component={TicketList} />
+        <Route path='/newticket' component={NewTicketForm} />
+        <Route component={Error404} />
+      </Switch>      
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header(){
   return (
@@ -11,6 +12,7 @@ function Header(){
           }
         `}</style>
       <h1>Help Queue!</h1>
+      <Link to="/">Home</Link> | <Link to="/newticket">Create Ticket</Link>
     </div>
   );
 }
