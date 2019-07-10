11eeef2c01e69ff68e8567e0545a83ddc6f1bf82
Student <example@example.com>
2019-07-10 16:16:30 -0700

    add advanced state structures and mutating state slices.

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Ticket(props){
-  return (
+  const ticketInformation =
     <div>
       <style jsx>{`
         div {
@@ -13,12 +13,11 @@ function Ticket(props){
       `}</style>
       <h3>{props.location} - {props.names}</h3>
       <h4>{props.formattedWaitTime}</h4>
-      <p><em>{props.issue}</em></p>
       <hr/>
     </div>;
   if (props.currentRouterPath === '/admin'){
     return (
-      <div onClick={() => {alert('hey, you just clicked the ticket belonging to ' + props.names);}}>
+      <div onClick={() => {props.onTicketSelection(props.ticketId);}}>
         {ticketInformation}
       </div>
     );
@@ -36,7 +35,9 @@ Ticket.propTypes = {
   location: PropTypes.string.isRequired,
   issue: PropTypes.string,
   formattedWaitTime: PropTypes.string.isRequired,
-  currentRouterPath: PropTypes.string
+  currentRouterPath: PropTypes.string,
+  onTicketSelection: PropTypes.func,
+  ticketId: PropTypes.string.isRequired
 };
 
 export default Ticket;
